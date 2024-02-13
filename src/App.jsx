d5f8a00d0fb7646d747fda501a54c58b6c961241
HOPMNC <andreylcm@gmail.com>
2024-02-13 18:01:13 +0600

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Header from "./components/Header/Header";
+
+function App() {
+  return <div className="wrapper">
+    <Header/>
+  </div>;
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import style from './header.module.scss';
+
+function Header() {
+  return <div className={style.header}></div>;
+}
+
+export default Header;
